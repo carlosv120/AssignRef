cf87087fcf176e5b42038a4c6f3c8b61c8d4f1dd
Carlos Vanegas <128112167+carlosv120@users.noreply.github.com>
2023-07-07 20:32:55 -0400

    Add files via upload

---
+++ dotNet/Models/Domain/Officials/BaseOfficial.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sabio.Models.Domain.Officials
+{
+    public class BaseOfficial
+    {
+        public int Id { get; set; }
+
+        public BaseUser User { get; set; }
+
+        public string Gender { get; set; }
+
+        public LookUp3Col Position { get; set; }
+    }
+}
---
+++ dotNet/Models/Domain/Officials/Official.cs
@@ -0,0 +1,23 @@
+﻿using Sabio.Models.Domain.Conferences;
+using Sabio.Models.Domain.Locations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sabio.Models.Domain.Officials
+{
+    public class Official : BaseOfficial
+    {
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public LookUp3Col PrimaryPosition { get; set; }
+        public Location Location { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateModified { get; set; }
+        public LookUp StatusType { get; set; }
+        public List<BaseConference> Conferences { get; set; }
+
+    }
+}
