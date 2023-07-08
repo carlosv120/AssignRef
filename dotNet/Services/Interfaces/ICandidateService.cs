b2e3dd0f6e48aa68c48d185354b9e376c9235040
Carlos Vanegas <128112167+carlosv120@users.noreply.github.com>
2023-07-07 20:36:20 -0400

    Add files via upload

---
+++ dotNet/Services/Interfaces/ICandidateService.cs
@@ -0,0 +1,26 @@
+﻿using Sabio.Models.Domain.Candidates;
+using Sabio.Models;
+using Sabio.Models.Requests.Candidates;
+
+namespace Sabio.Services.Interfaces
+{
+    public interface ICandidateService
+    {
+        int Add(CandidateAddRequest model, int userId);
+
+        Paged<Candidate> SelectAll(int pageIndex, int pageSize, int statusTypeId, int conferenceId);
+
+        Paged<Candidate> SearchPaginationByConferenceId(int pageIndex, int pageSize, int query);
+
+        Paged<Candidate> SearchPaginationByPositionId(int pageIndex, int pageSize, int query);
+
+        Paged<Candidate> SearchPaginationByUserId(int pageIndex, int pageSize, int query);
+
+        Paged<Candidate> SearchPaginationByName(int pageIndex, int pageSize, string query, int statusTypeId, int conferenceId);
+
+        void Update(CandidateUpdateRequest model);
+
+        void Delete(int id);
+
+    }
+}
---
+++ dotNet/Services/Interfaces/IOfficialService.cs
@@ -0,0 +1,29 @@
+﻿using Sabio.Models;
+using Sabio.Models.Domain.Officials;
+using Sabio.Models.Requests.Officials;
+using System.Collections.Generic;
+
+namespace Sabio.Services.Interfaces
+{
+    public interface IOfficialService
+    {
+        int Add(OfficialAddRequest model , int userId);
+
+        void Delete(int Id);
+
+        Official GetByUser(int userId);
+
+        List<Official> GetByConference(int conferenceId);
+
+        List<Official> GetByPosition(int positionId);
+
+        Paged<Official> GetAll(int pageIndex, int pageSize);
+
+        Paged<Official> Search(int pageIndex, int pageSize, string query);
+
+        void Update(OfficialUpdateRequest model);
+
+        List<BaseOfficial> GetByMissingCertification(int conferenceId, int certificationId);
+
+    }
+}
