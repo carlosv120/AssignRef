a49b880b843494bdbae34699acf0bcd07bc6d6fb
Carlos Vanegas <128112167+carlosv120@users.noreply.github.com>
2023-07-07 20:34:29 -0400

    Add files via upload

---
+++ dotNet/Models/Requests/Officials/OfficialAddRequest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sabio.Models.Requests.Officials
+{
+    public class OfficialAddRequest
+    {
+        [Required]
+        public int UserId { get; set; }
+
+        [Required]
+        public int PrimaryPositionId { get; set; }
+
+        [Required]
+        public int LocationId { get; set; }
+
+    }
+}
